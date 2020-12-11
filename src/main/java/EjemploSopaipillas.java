ce4276a92f7bbd6cbe0bb5315f5a70a401d42530
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-12-11 11:08:00 -0300

    Implemento los métodos y atributos de la primera parte

--- src/main/java/EjemploSopaipillas.java
+++ src/main/java/EjemploSopaipillas.java
@@ -1,5 +1,15 @@
 public class EjemploSopaipillas {
     public static void main(String[] args) {
         VendedorSopaipillas miVendedor = new VendedorSopaipillas("Juan");
+        miVendedor.saludar();
+        miVendedor.cocinarSopaipillas(10);
+        System.out.println(miVendedor.sopaipillasEstanDisponibles());
+        miVendedor.echarMayonesa();
+        miVendedor.echarKetchup();
+        miVendedor.echarAji();
+        miVendedor.venderSopaipillas(3);
+        miVendedor.despedirse();
+        miVendedor.recibirMonedasDe100(2);
+        miVendedor.recibirMonedasDe50(1);
     }
 }
